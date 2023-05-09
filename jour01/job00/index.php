dcf8d7586386289f79eb56bec213e2ac160ec945
neggy-nettah <neggy.nettah@laplateforme.io>
2023-05-09 12:07:44 +0200

    Job01

--- jour01/job00/index.php
+++ jour01/job00/index.php
@@ -1,5 +1,23 @@
 <?php
 
-    echo "hello Laplateforme!";
+    echo "hello Laplateforme! ";
+    
+    $str = "LaPlateforme";
+    $str2 = "Vive";
+    $str3 = "!";
+    echo $str2 . " " . $str . " " . $str3;
+
+    $val = 6;
+    echo $val;
+    echo "/";
+    $val = $val + 4;
+    echo $val;
+
+    $myBool = true; 
+    echo $myBool;
+
+    $myBool = false; 
+    echo $myBool;
+
 
 ?>
