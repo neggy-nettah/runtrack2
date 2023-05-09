6c6ee20ffc96a7506c0be65c757d6f809c932128
neggy-nettah <neggy.nettah@laplateforme.io>
2023-05-09 12:50:07 +0200

    Job03

---
+++ jour01/job03/index.php
@@ -0,0 +1,38 @@
+<?php
+    
+    // String:
+    $modele = "Peugeot 308.";
+    echo $modele;
+    echo "\n";
+
+
+    // Integer:
+    $prix = 10 ;
+    echo $prix;
+    echo "\n";
+
+    //Float:
+    $chevaux = 139.9;
+    echo $chevaux;
+    echo "\n";
+
+    //Boolean:
+    $dispo = true;
+    echo $dispo;
+
+    //Array
+    $marques = array("Peugeot", "Renault", "Citroen");
+    
+    //Object
+    //$voiture = new Voiture("Peugeot", "308", 15000);
+
+
+    //Resource
+    //$baseDeDonnees = mysqli_connect("localhost", "utilisateur", "motdepasse", "ma_base_de_donnees");
+
+    //J'ai commenté "Resource et Object" car il faut plus d'informations pour que le code soit valide.
+
+
+
+
+?>
